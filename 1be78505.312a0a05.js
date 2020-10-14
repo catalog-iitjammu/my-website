f1ad97ed1b264e1c0b18f539d7c4263597cac71a
Technical Officer <to@mailman.iitjammu.ac.in>
2020-10-14 12:28:07 +0530

    Deploy website - based on HEAD

---
+++ 1be78505.312a0a05.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{109:function(n,o,w){"use strict";var a=w(104);w.d(o,"a",(function(){return a.a}))}}]);
--- a0331fdb.5ec5805e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{81:function(s){s.exports=JSON.parse('{"allTagsPath":"/my-website/blog/tags","slug":"docs","name":"docs","count":1,"permalink":"/my-website/blog/tags/docs"}')}}]);
